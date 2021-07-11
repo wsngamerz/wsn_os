5d203cd7d05bf811b223b4f2dce0e57118edc4d7
William Neild <wneild5@gmail.com>
2021-07-11 12:54:07 +0100

    added debug constant so that qemu doesnt "oversize"

--- bootloader/src/gop.rs
+++ bootloader/src/gop.rs
@@ -2,8 +2,15 @@ use log::info;
 use uefi::prelude::{Boot, ResultExt, SystemTable};
 use uefi::proto::console::gop::GraphicsOutput;
 
+// A Debug value which prevents qemu from setting an insane sized value cause for
+// some reason even on a 1080p display, it attempts to set the max size at about
+// a 4k res
+const RESIZE_1280_720: bool = true;
+
 pub struct GopData {
     framebuffer_pointer: *mut u8,
+    width: usize,
+    height: usize,
 }
 
 pub fn setup_gop(st: &SystemTable<Boot>) -> GopData {
@@ -24,14 +31,19 @@ pub fn setup_gop(st: &SystemTable<Boot>) -> GopData {
         let mode_info = mode.info();
         let mode_resolution = mode_info.resolution();
         let res = mode_resolution.0 * mode_resolution.1;
-        if res >= largest_size {
+
+        if RESIZE_1280_720 {
+            if mode_resolution.0 == 1280 && mode_resolution.1 == 720 {
+                final_mode = mode;
+            }
+        } else if res >= largest_size {
             final_mode = mode;
         }
 
         info!(">> #{} // [{}] // {:?}", index, res, mode_resolution);
     }
 
-    // found the largest mode
+    // found the largest mode so set it
     info!("Largest: {:?}", final_mode.info());
     gop.set_mode(&final_mode)
         .expect_success("Failed to change GOP mode");
@@ -41,5 +53,7 @@ pub fn setup_gop(st: &SystemTable<Boot>) -> GopData {
 
     return GopData {
         framebuffer_pointer: gop.frame_buffer().as_mut_ptr(),
+        width: final_mode.info().resolution().0,
+        height: final_mode.info().resolution().1,
     };
 }
